4a6f51a997d34dbed524031d787385603cc1f11c
Fadlan Fadilah <fhadlan123@gmail.com>
2023-08-20 16:05:49 +0700

    posting selesai

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -2,10 +2,12 @@ import { Box, useMediaQuery } from "@mui/material";
 import { useSelector } from "react-redux";
 import NavBar from "scenes/navBar";
 import UserWidget from "scenes/widgets/UserWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
 
 const HomePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
-  const { _id } = useSelector((state) => state.user);
+  const { _id, picturePath } = useSelector((state) => state.user);
   return (
     <Box>
       <NavBar />
@@ -22,7 +24,10 @@ const HomePage = () => {
         <Box
           flexBasis={isNonMobileScreens ? "42%" : undefined}
           mt={isNonMobileScreens ? undefined : "2rem"}
-        ></Box>
+        >
+          <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
+        </Box>
         {isNonMobileScreens && <Box flexBasis="26%"></Box>}
       </Box>
     </Box>
--- src/state/index.js
+++ src/state/index.js
@@ -22,7 +22,7 @@ export const authSlice = createSlice({
       state.user = null;
       state.token = null;
     },
-    setFriend: (state, action) => {
+    setFriends: (state, action) => {
       if (state.user) {
         state.user.friends = action.payload.friends;
       } else {
@@ -42,6 +42,6 @@ export const authSlice = createSlice({
   },
 });
 
-export const { setMode, setLogin, setLogout, setFriend, setPosts, setPost } =
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } =
   authSlice.actions;
 export default authSlice.reducer;
